c0504d1dbf3532ce5ccf0bd337c5421367d6468d
Harold Bernethe Soto González <bernethe@gmail.com>
2021-10-19 15:23:33 -0600

    second commit

---
+++ foodexpress.js
@@ -0,0 +1,1 @@
+'use strict';var main=function(){var a=document.body.dataset.page;window[a]&&"function"==typeof window[a]&&window[a]()};document.addEventListener("DOMContentLoaded",main,!1);
--- src/app.js
+++ src/app.js
@@ -1,25 +1,10 @@
 'use strict';
 
 const main = () => {
-	window.addEventListener('scroll', () => {
-		if(window.scrollY > 40) {
-			document.querySelector('.main-nav').classList.add('is-pinned');
-		} else {
-			document.querySelector('.main-nav').classList.remove('is-pinned');
-		}
-	}, !1);
-
-	document.getElementById('menu_btn').addEventListener('click', toggleMenu, !1);
-
-	addEventListenerList(document.querySelectorAll('nav.main-nav a'), 'click', closeMenu);
-
-	addEventListenerList(document.querySelectorAll('#proyectos a'), 'click', showBigPicture);
-
-	document.getElementById('contactForm').addEventListener('submit', formEval, !1);
-
-	document.getElementById('tyc_anchor').addEventListener('click', tycShow, !1);
-
-	AOS.init();
+	let _cp = document.body.dataset.page;
+	if (window[_cp] && typeof window[_cp] === 'function') {
+		window[_cp]();
+	}
 };
 
 document.addEventListener('DOMContentLoaded', main, !1);
